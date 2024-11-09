1a15df32b6f0fce8d3433ac7fb8dc08cb6e6347c
VeruDoraZ <163351959+VeruDoraZ@users.noreply.github.com>
2024-11-09 14:28:01 +0300

    Delete Brainy directory

--- Brainy/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('quizzes/', views.tours, name='quiz'),
-    path('quizzes/', views.tour_detail, name='quiz_detail'),
-]
